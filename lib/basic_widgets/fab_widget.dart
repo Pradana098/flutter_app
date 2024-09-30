ef5f0c96f8291b3851bd0e971d14a2a32b69ad44
pradadimas534@gmail.com <pradanadimas534@gmail.com>
2024-09-30 21:13:39 +0700

    tes flutter 2

--- lib/basic_widgets/fab_widget.dart
+++ lib/basic_widgets/fab_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoadingCupertino extends StatelessWidget {
-  const LoadingCupertino({Key? key}) : super(key: key);
+class FabWidget extends StatelessWidget {
+  const FabWidget({Key? key}) : super(key: key);
   
   @override
   Widget build(BuildContext context) {
